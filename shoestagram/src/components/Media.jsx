6ea30cbbc2f2d1704a480d10450bf6b2f5a02709
Coco Moloko <courtneyb.brecht@gmail.com>
2017-02-22 18:47:14 +0000

    added buy online and instore display for Media page

--- shoestagram/src/components/Media.jsx
+++ shoestagram/src/components/Media.jsx
@@ -4,16 +4,36 @@ import '../stylesheets/media.css';
 class Media extends React.Component {
   render() {
     return (
-        <div className="mediaAll">
-            <div className="mediaJumboPic">JumboImage Goes Here </div>
-            <h2 className="headerStyle">PRODUCT DESCRIPTION GOES HERE SUPER LONG TEST WOWOWOWOWO</h2>
-            <p className="parStyle">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
-            </p>
-        <div className="buttonRow">
-            <div className="buyOnline">Buy Online</div>
-            <div className="buyInStore">Buy In-Store</div>
+      <div className="mediaAll">
+          <div className="mediaJumboPic">JumboImage Goes Here </div>
+          <h2 className="headerStyle">PRODUCT DESCRIPTION GOES HERE SUPER LONG TEST WOWOWOWOWO</h2>
+          <p className="parStyle">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+          </p>
+      <div className="buttonRow">
+          <div className="buyOnline">Buy Online</div>
+          <div className="buyInStore">Buy In-Store</div>
+      </div>
+      
+        <div className="accordionLinks">
+            <div className="linkDiv">
+                  <div className="words">
+                    <p className="productName">from Provider goes here blah blah blah super long just in case wow its still going I cantfdsf fdfsdf fdfs</p>
+                  </div>
+                <h3 className="priceDiv">$200</h3>
+                <div className="linkButton"><i className="fa fa-arrow-right fa-2x" aria-hidden="true"></i></div>
+            </div>
         </div>
+          <div className="accordionLinks">
+            <div className="linkDiv">
+                  <div className="words">
+                    <p className="productName">from Ebay</p>
+                  </div>
+                <div className="linkButton"><i className="fa fa-arrow-right fa-2x" aria-hidden="true"></i></div>
+            </div>
         </div>
+        
+        <div className="googleMapsContainer"> Map container goes here</div>
+      </div>
     );
   }
 }
