fac1574a4e5cc8b1ddc0529109e1fe07051c615b
Coco Moloko <courtneyb.brecht@gmail.com>
2017-02-22 20:01:41 +0000

    added Like button

--- shoestagram/src/components/Media.jsx
+++ shoestagram/src/components/Media.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import '../stylesheets/media.css';
 
+//to change Like button on click <i className="fa fa-thumbs-up" aria-hidden="true"></i>
+
 class Media extends React.Component {
   render() {
     return (
       <div className="mediaAll">
           <div className="mediaJumboPic">JumboImage Goes Here </div>
+          
+          <div className="likeButton"><i className="fa fa-thumbs-o-up fa-2x" aria-hidden="true"></i></div>
           <h2 className="headerStyle">PRODUCT DESCRIPTION GOES HERE SUPER LONG TEST WOWOWOWOWO</h2>
           <p className="parStyle">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
           </p>
+          
       <div className="buttonRow">
           <div className="buyOnline">Buy Online</div>
           <div className="buyInStore">Buy In-Store</div>
@@ -32,7 +37,7 @@ class Media extends React.Component {
             </div>
         </div>
         
-        <div className="googleMapsContainer"> Map container goes here</div>
+        <div className="googleMapsContainer">Map container goes here</div>
       </div>
     );
   }
