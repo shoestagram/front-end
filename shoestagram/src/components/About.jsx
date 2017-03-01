39cffe56454e12802fdcab1f09674af336e44eed
Coco Moloko <courtneyb.brecht@gmail.com>
2017-03-01 16:10:23 +0000

    finished about page and styling

--- shoestagram/src/components/About.jsx
+++ shoestagram/src/components/About.jsx
@@ -1,6 +1,5 @@
 import NavNoSearch from './NavNoSearch';
 import Footer from './Footer';
-import { Link } from 'react-router';
 import React from 'react';
 import '../stylesheets/about.css';
 
@@ -13,7 +12,7 @@ export class About extends React.Component {
           <NavNoSearch />
           <a href="https://github.com/shoestagram" className="headerRow">
               <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-               <h2 className="mainHeader">See Our Repos on Github</h2>
+               <h2 className="mainHeader">See Shoestagram's Repos</h2>
           </a>
           
             <h2 className="midHeader">Meet The Crew:</h2>
@@ -22,11 +21,11 @@ export class About extends React.Component {
                 <img alt="@coco-moloko" className="avatar" height="100" src="https://avatars2.githubusercontent.com/u/15265338?v=3&amp;s=400" width="100"/>
                 <p className="aboutUs"><strong>Name:</strong> Coco Brecht
                 <br />
-                <strong>Created:</strong> Front End, Logo Design</p>
+                <strong>Created:</strong> Front End, UX Design</p>
 
                   <a href="https://github.com/coco-moloko">Github</a>
                   <a href="https://www.linkedin.com/in/coco%E2%80%8B-brecht/">Linkedin</a>
-                  <a href="mailto:cocobrecht@gmail?Subject=Shoestagram" target="_top">Send Email</a>
+                  <a href="mailto:cocobrecht@gmail.com" target="_top">Email</a>
               </div>
               
               <div className="personDiv">
@@ -37,18 +36,18 @@ export class About extends React.Component {
 
                   <a href="https://github.com/MelofunkCodes">Github</a>
                   <a href="https://www.linkedin.com/in/melinda-yeoh">Linkedin</a>
-                  <a href="mailto:myeoh27@gmail.com?Subject=Shoestagram" target="_top">Send Email</a>
+                  <a href="mailto:myeoh27@gmail.com" target="_top">Email</a>
               </div>
                 
               <div className="personDiv">
-                <img alt="@coco-moloko" className="avatar" height="100" src="https://avatars2.githubusercontent.com/u/15265338?v=3&amp;s=400" width="100"/>
-                <p className="aboutUs"><strong>Name:</strong> Coco Brecht
+                <img alt="@coco-moloko" className="avatar" height="100" src="https://avatars3.githubusercontent.com/u/22350327?v=3&s=400" width="100"/>
+                <p className="aboutUs"><strong>Name:</strong> Allen Lee
                 <br />
-                <strong>Created:</strong> Front End, Logo Design</p>
+                <strong>Created:</strong> Back End, Database</p>
 
-                  <a href="https://github.com/coco-moloko">Github</a>
-                  <a href="https://www.linkedin.com/in/coco%E2%80%8B-brecht/">Linkedin</a>
-                  <a href="mailto:cocobrecht@gmail.com?Subject=Shoestagram" target="_top">Send Email</a>
+                  <a href="https://github.com/allendecodemtl">Github</a>
+                  <a href="https://www.linkedin.com/in/allenleechowpan/">Linkedin</a>
+                  <a href="mailto:allen@leechowpan.com" target="_top">Email</a>
               </div>
               
           <Footer />
